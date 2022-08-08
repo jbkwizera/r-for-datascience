7a02814922742e6613d809e467a592cd32c3f294
jbkwizera <jeanbaptistekwi@gmail.com>
2022-08-09 00:01:35 +0200

    Book example 'gather'

--- tidyr.R
+++ tidyr.R
@@ -16,6 +16,33 @@ df <- df %>% mutate(across(!(country), as.integer))
 (df %>% group_by(year) %>% summarize(n = sum(cases)))
 (df %>% count(year, wt = cases))
 
+# Book example of NOT tidy data (1). 1999 and 2000 should be values
+# of a column "year". Some they must be gathered together.
+# A new column must be created to hold the values, which are cases
+tb4a <- tribble(
+       ~country, ~`1999`, ~`2000`,
+  "Afghanistan",     745,    2666,
+       "Brazil",   37735,   80488,
+        "China",  212258,  213766)
+
+tb4a_tidy <- tb4a %>%
+  gather(`1999`, `2000`, key = "year", value = "cases") %>%
+  mutate(across(-country, as.integer))
+
+# Book example of NOT tidy data (1). 1999 and 2000 should be values
+# of a column "year". Some they must be gathered together.
+# A new column must be created to hold the values, which are population
+tb4b <- tribble(
+       ~country,     ~`1999`,      ~`2000`,
+  "Afghanistan",    19987071,     20595360,
+       "Brazil",   172006362,    174504898,
+        "China",  1272915272,   1280428583)
+tb4b_tidy <- tb4b %>% 
+  gather(`1999`, `2000`, key = "year", value = "population") %>%
+  mutate(across(-country, as.integer))
+
+tb4 <- inner_join(tb4a_tidy, tb4b_tidy, by = c("country", "year"))
+
 # `Total Male` and `Total Female` are sample sizes of the respective genders
 # from the population size `Total population`
 # This is NOT tidy data. There is clearly the need for gender and sample columns
@@ -28,8 +55,7 @@ population <- tribble(
        4,           1,        126217,          193293,              319510,
        4,           2,        468454,          732914,             1201368,
        3,           1,         68993,           71217,              140210,
-       3,           2,        166143,          160136,              326279
-)
+       3,           2,        166143,          160136,              326279)
 
 # This IS tidy data. In fact, the population column could be removed since
 # it can easily be calculated from the gender_population
